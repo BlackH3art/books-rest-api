ff1a32cbe8fdea96dbe9f25505f548d4dde5a206
BlackH3art <jaroslaw.musielak@outlook.com>
2023-03-13 11:11:50 +0100

    add: implement deleteBook controller

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -10,7 +10,7 @@ export const getAllBooks = async (req: Request, res: Response) => {
   try {
     const books = await AppDataSource.manager.find(Book);
     res.status(200).json(books);
-    
+
   } catch (error) {
     res.status(500).json({ msg: "Error while getting books", data: error });
   }
@@ -30,4 +30,18 @@ export const addBook = async (req: Request, res: Response) => {
   }  
 }
 
-export const deleteBook = async () => {}
+export const deleteBook = async (req: Request, res: Response) => {
+
+  const { id } = req.params;
+
+  try {
+    const book = await AppDataSource.manager.findBy(Book, { id: id});
+    if(book.length === 0) return res.status(404).json({ msg: "Not found" });
+
+    await AppDataSource.manager.delete(Book, book[0]);
+    res.status(204).json();
+
+  } catch (error) {
+    res.status(500).json({ msg: "Error while deleting", data: error });
+  }
+}
