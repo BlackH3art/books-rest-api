19c3adbd9f066c6fed03e5016116b63c85526e5d
BlackH3art <jaroslaw.musielak@outlook.com>
2023-03-13 10:31:18 +0100

    add: implement getAllBooks controller

--- src/controllers/booksController.ts
+++ src/controllers/booksController.ts
@@ -1,10 +1,20 @@
 import { Request, Response } from "express";
+import { AppDataSource } from "../data-source";
 import { Book } from "../db/entities/Book.entity";
 
 import { BookDataInterface } from "../interfaces/BookDataInterface";
 
 
-export const getAllBooks = async () => {}
+export const getAllBooks = async (req: Request, res: Response) => {
+
+  try {
+    const books = await AppDataSource.manager.find(Book);
+    res.status(200).json(books);
+    
+  } catch (error) {
+    res.status(500).json({ msg: "Error while getting books", data: error });
+  }
+}
 
 export const addBook = async (req: Request, res: Response) => {
 
